0012443f69f0dc12bb8fb91d2134c3db7f8cc73d
Roumen Georgiev <roumen_georgiev@yahoo.com>
2023-03-31 10:34:08 +0300

    ask

--- script.sh
+++ script.sh
@@ -35,8 +35,16 @@ git fetch origin master
 # merge the latest changes from the master branch into our current branch
 git merge origin/master
 
-# clear all lines containing console.log in the modified files
-git diff --name-only HEAD^..HEAD | xargs sed -i '/console.log/d'
+# ask user if they want to delete console.log lines
+read -p "Do you want to delete all lines containing console.log? [Y/n] " delete_choice
+delete_choice=${delete_choice:-Y}
+delete_choice=$(echo "$delete_choice" | tr '[:upper:]' '[:lower:]')
+
+if [[ $delete_choice == "y" ]]; then
+    # clear all lines containing console.log in the modified files
+    git diff --name-only master..HEAD | xargs sed -i '/console.log/d'
+fi
+
 
 # ask the user which version to increment (default: minor)
 read -p "Increment major, minor or patch version? (default: minor) " increment_option
